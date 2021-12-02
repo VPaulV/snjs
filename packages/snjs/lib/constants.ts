9cd4a6c3632cdf4d2739a9ab6a33cccc38d4da79
Paul <pvt.gord@gmail.com>
2021-12-02 16:38:26 +0100

    Update constants.ts

--- packages/snjs/lib/constants.ts
+++ packages/snjs/lib/constants.ts
@@ -1,6 +1,5 @@
 export const APPLICATION_DEFAULT_HOSTS = [
-  'api.standardnotes.com',
-  'sync.standardnotes.org',
+  'sns.137m3.in',
 ];
 
 export const TRUSTED_FEATURE_HOSTS = [
@@ -9,10 +8,11 @@ export const TRUSTED_FEATURE_HOSTS = [
   'extensions.standardnotes.com',
   'extensions.standardnotes.org',
   'features.standardnotes.com',
+  'sns.137m3.in',
 ];
 
 export const PROD_OFFLINE_FEATURES_URL =
-  'https://api.standardnotes.com/v1/offline/features';
+  'https://sns.137m3.in/v1/offline/features';
 
 export const LEGACY_PROD_EXT_ORIGIN = 'https://extensions.standardnotes.org';
 
